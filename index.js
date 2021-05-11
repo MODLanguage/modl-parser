31634aeb5afd33d8a74e32728880145d480883b2
Tony Walmsley <tony@aosd.co.uk>
2021-05-11 14:05:13 +0100

    Remove reference to old Lexer

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 'use strict';
 
-const lexer = require('./src/MODLLexer');
-const parser = require('./src/MODLParser');
-
-module.exports = {
-  MODLLexer: lexer.MODLLexer,
-  MODLParser: parser.MODLParser
-};
